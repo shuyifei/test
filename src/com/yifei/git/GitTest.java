bb9e17e2da81a37781f0e4ccb2bfbd714e9464ea
shuyifei <54239977+shuyifei@users.noreply.github.com>
2023-01-14 11:26:10 -0600

    pull commit

--- src/com/yifei/git/GitTest.java
+++ src/com/yifei/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix commit1");
         System.out.println("push test");
+        System.out.println("pull commit");
     }
 }
